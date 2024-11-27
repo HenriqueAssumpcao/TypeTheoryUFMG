f6e00dd6a6169592c267bd71ab74cbe5a8a90361
Csaba Schneider <csaba.schneider@gmail.com>
2024-11-27 08:35:56 -0300

    added ex5_10

--- TypeTheoryUFMG/ch5.lean
+++ TypeTheoryUFMG/ch5.lean
@@ -9,3 +9,21 @@ theorem sol
   exact λ a : P x => λ _ : Q x => a
 
 end c5ex8
+
+
+namespace c5ex10
+-- Exercício 5.10
+-- Code by Csaba
+
+theorem ex5_10
+  (S : Prop)
+  (P : S → Prop)
+  (f : S → S)
+  (g : S → S)
+  (u : (x : S) → P (f x) → P (g x))
+  (v : (x y : S) → (P x → P y) → P (f x)) :
+  (x : S) → P (f (f x)) := by
+
+  intro (x : S)
+  exact v (f x) (g x) (u x)
+end c5ex10

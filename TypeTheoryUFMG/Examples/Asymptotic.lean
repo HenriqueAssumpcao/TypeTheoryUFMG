1f148ae08b698a2e4692a8504725a18cc7d17757
bernborgess <bernborgess@outlook.com>
2024-11-30 13:20:45 -0300

    Simplify assymptotic proof with `calc` block

--- TypeTheoryUFMG/Examples/Asymptotic.lean
+++ TypeTheoryUFMG/Examples/Asymptotic.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Nat.Notation
 import Mathlib.Tactic.NthRewrite
+import Mathlib.Tactic.Set
 import Mathlib.Tactic.Use
 
 namespace Asymptotic
@@ -13,12 +14,6 @@ def O (f g : ℕ → ℕ) : Prop :=
   → f n <= c * g n
 
 #check O (λx↦x) (λx↦x)
-#check Nat.le_trans
-#check Nat.add_mul
-#check Nat.one_mul
-#check Nat.add_le_add_left
-#check Nat.le_of_mul_le_mul_left
-#check Nat.mul_le_mul_left
 
 -- Prove:
 -- n² + 2n is O(n²)
@@ -28,16 +23,10 @@ example : O (λn↦n^2 + 2*n) (λn↦n^2) := by
   use 3
   use 2
   intro n nge2
-  apply @Nat.le_trans (n ^ 2 + 2 * n) (n ^ 2 + 2 * n ^ 2) (3 * n ^ 2)
-  . apply Nat.add_le_add_left
-    apply Nat.mul_le_mul_left
-    rw [Nat.pow_two]
-    nth_rewrite 1 [←Nat.one_mul n]
-    apply Nat.mul_le_mul_right
-    exact Nat.le_of_succ_le nge2
-  . apply Nat.le_of_eq
-    nth_rewrite 1 [←Nat.one_mul (n^2)]
-    rw [←Nat.add_mul]
-
+  calc
+   _ = n ^ 2 + 2 * n  := rfl
+   _ ≤ n ^ 2 + n ^ 2  := by simp [Nat.pow_two, Nat.mul_le_mul_right n nge2]
+   _ = 2 * n ^ 2      := by simp [Nat.two_mul]
+   _ ≤ 3 * n ^ 2      := by simp [Nat.mul_le_mul]
 
 end Asymptotic

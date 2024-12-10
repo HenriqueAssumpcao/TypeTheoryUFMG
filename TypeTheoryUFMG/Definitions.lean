78dc0df7e9a6bab6a3307d00cb1a1917177916b6
bernborgess <bernborgess@outlook.com>
2024-12-10 16:28:52 -0300

    8.4 Instantiations of definitions

--- TypeTheoryUFMG/Definitions.lean
+++ TypeTheoryUFMG/Definitions.lean
@@ -42,6 +42,41 @@ def D (n : ℕ+) := Nat.divisors n
 example : k ∣ l → D k ⊆ D l := sorry
 
 
+-- # 8.4 Instantiations of definitions
+-- Obviously, definitions are not made for their own sake, but in order to be used .
+-- For example, we have already mentioned that the definition c := (1+√5) / 2 may
+-- be used to state that c^2 − c = 1, which is easier to read than
+-- (1+√5) / 2)^2 − (1+√5)/2 = 1 .
+-- Moreover, the same c may be used over and over again, for example in the following calculation:
+-- ‘Since c2 = c + 1, we have that c^3 = c^2 + c = c + 1 + c = 2c + 1 ’,
+example (h : c^2 = c + 1) : c^3 = 2*c + 1
+  := by calc
+    _ = c ^ 3      := rfl
+    _ = c^2 + c    := sorry
+    _ = c + 1 + c  := congrFun (congrArg HAdd.hAdd h) c
+    _ = 2*c + 1    := sorry
+
+-- or in establishing that
+-- ‘The n-th Fibonacci number fn satisfies the equation fn = (c^n−(1−c)^n)/√5 .’
+
+-- ‘The function sending a real number to its third power, is increasing.’
+example : Increasing (λ x : ℝ ↦ x^3) := sorry
+-- One says: ‘f has been instantiated with λx : R . x3 ’
+
+-- For example, let’s take for S the set R of the reals, and for R the relation ‘≤’
+-- on that set R. Then we obtain the correct instantiation
+-- total (R, ≤) .
+#check @Total ℝ (·≤·)
+-- This is a proposition, which does indeed hold in mathematics.
+
+-- Instantiating S with N+, the positive naturals, and R with ‘|’, the divisibility
+-- relation on N+, we obtain
+-- total (N+, | ).
+#check @Total ℕ+ (·∣·)
+
+-- As an instantiation, this is correct, again. (Which has got nothing to do with
+-- the fact that, as a proposition, it is false: for example, neither 3|5, nor 5|3.)
+
 
 
 end TypeTheoryUFMG

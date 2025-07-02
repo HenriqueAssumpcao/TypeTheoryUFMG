e15d146651052a3e76d45eb33812980aecdbf074
schcs <csaba.schneider@gmail.com>
2025-07-02 16:12:17 -0300

    added file for chapter 2 exercises

--- TypeTheoryUFMG/chapter2.lean
+++ TypeTheoryUFMG/chapter2.lean
@@ -3,4 +3,13 @@ variable (a: α)
 
 
 def myLambda : α → α := fun x => x
-def
+-- Solution to Exercise 11 in the book Type Theory and Formal Proof
+
+example  : ((α → γ) → α) → (α → γ) → ( β → γ ) :=
+  fun (g : (α → γ) → α) =>  (fun (a : α → γ) => fun (_ : β) => a (g a))
+
+
+def  func1 (g : (α → γ) → α) : (α → γ) → ( β → γ ) :=
+  fun (a : α → γ) => fun (_ : β) => a (g a)
+
+#check func1

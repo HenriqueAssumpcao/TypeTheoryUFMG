b89b0bc4e3b2f28a5ec2ebf14110aa8982b52a4f
schcs <csaba.schneider@gmail.com>
2025-08-27 14:51:34 -0300

    added to chapter 3

--- TypeTheoryUFMG/chapter3.lean
+++ TypeTheoryUFMG/chapter3.lean
@@ -5,7 +5,20 @@ variable (nat bool : Type)
   Rob Nederpelt, Herman Geuvers, and Fredrik de Vries.
 -/
 
--- M ≡λα,β,γ:∗.λf : α →β.λg:β →γ.λx:α.g(fx)
+
+/- Exercise 3.2
+
+Give a full (i.e. not-shortened) derivation in λ2 to show that the following
+term is legal; use the ﬂag format.
+
+M ≡λα,β,γ:∗. λf:α →β. λg:β →γ. λx:α. g(fx)
+
+--/
+namespace ex_32
+def M (α β γ : Type) (f : α → β) (g : β → γ) (x : α) := g (f x)
+example : ∀ (α β γ : Type), (α → β) →  (β → γ) → α → γ := M
+
+end ex_32
 
 /-Exercise 3.3
   Take M as in Exercise 3.2. Assume nat : ∗, bool : ∗, suc : nat → nat and even : nat → bool.
@@ -15,29 +28,66 @@ variable (nat bool : Type)
     (2) directly.
 -/
 
+namespace ex_33
+def M (α β γ : Type) (f : α → β) (g : β → γ) (x : α) := g (f x)
 -- solution for (a)
 variable (suc : nat → nat) (even : nat → bool)
-
-def M := λ(α β γ : Type) => λ(f : α → β) => λ(g : β → γ) => λ(x : α) => g (f x)
-
 def term_3_3_a := M nat nat bool suc even
 
 -- solution for (b)
 
 --(1)
--- apllying β-reduction we conclude that the term_3_3_a is exactally λx : nat . even(suc x), so it is legal.
+-- applying β-reduction we conclude that the term_3_3_a is exactally λx : nat . even(suc x), so it is legal.
 
 --(2)
+
 def term_3_3_b := λ(x : nat) => even (suc x)
 
 -- ################################################################
 
-
-/-Exercise 3.4
+end ex_33
+/-
+Exercise 3.4
 Give a shortened derivation in λ2 to show that the following term is legal in the context Γ ≡ nat : ∗, bool : ∗ :
   (λα, β : ∗. λf : α → α. λg : α → β. λx : α. g(f(fx))) nat bool.
 -/
 
 -- solution
-
+namespace ex_34
 def term_3_4 := (λ(α β : Type) => λ(f : α → α) => λ(g : α → β) => λ(x : α) => g (f (f x))) nat bool
+
+end ex_34
+
+/-
+Exercise 3.5
+Let ⊥ ≡ Πα : ∗ . α and Γ ≡ β : ∗, x : ⊥.
+
+(a) Prove that ⊥ is legal.
+(b) Find an inhabitant of β in context Γ.
+(c) Give three not β-convertible inhabitants of β → β in context Γ, each
+in β-normal form.
+(d) Prove that the following terms inhabit the same type in context Γ:
+λf : β → β → β . f (x β)(x β), x((β → β → β) → β)-/
+
+namespace ex_35
+variable(x:Q) (β: Prop)
+
+-- solution for (a)
+def  Q :=  ∀ A : Prop, A
+example : Prop := Q
+
+
+-- solution for (b)
+
+example : β := x β
+
+-- solution for (c)
+
+example : β → β := x (β → β)
+example : β → β := λ(y : β) => y
+example : β → β := λ(y : β) => x (β → β) y
+
+def term1 := λ(f : β → β → β) => f (x β) (x β)
+def term2 := x ((β → β → β) → β)
+
+example : (β → β → β) → β := term1

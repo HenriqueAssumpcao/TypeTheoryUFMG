f62818a33707a53e507af652492c1832bfc410c5
TSF-Guilherme <ghds@aluno.ifnmg.edu.br>
2025-09-10 19:03:49 -0300

    Added solution up to exercise 3.7

--- TypeTheoryUFMG/chapter3.lean
+++ TypeTheoryUFMG/chapter3.lean
@@ -1,4 +1,3 @@
-variable (nat bool : Type)
 
 /-
   Exercises for chapter 3 of the book 'Type Theory and Formal Proof' by
@@ -11,7 +10,7 @@ variable (nat bool : Type)
 Give a full (i.e. not-shortened) derivation in λ2 to show that the following
 term is legal; use the ﬂag format.
 
-M ≡λα,β,γ:∗. λf:α →β. λg:β →γ. λx:α. g(fx)
+M ≡ λ α,β,γ : ∗. λ f : α → β. λ g : β → γ. λ x : α. g(fx)
 
 --/
 namespace ex_32
@@ -29,10 +28,14 @@ end ex_32
 -/
 
 namespace ex_33
+section
+
+variable (nat bool : Type) (suc : nat → nat) (even : nat → bool)
+
 def M (α β γ : Type) (f : α → β) (g : β → γ) (x : α) := g (f x)
+
 -- solution for (a)
-variable (suc : nat → nat) (even : nat → bool)
-def term_3_3_a := M nat nat bool suc even
+def term_3_3_a {nat bool : Type} {suc : nat → nat} {even : nat → bool} := M nat nat bool suc even
 
 -- solution for (b)
 
@@ -45,18 +48,22 @@ def term_3_3_b := λ(x : nat) => even (suc x)
 
 -- ################################################################
 
+end
 end ex_33
+
 /-
 Exercise 3.4
-Give a shortened derivation in λ2 to show that the following term is legal in the context Γ ≡ nat : ∗, bool : ∗ :
+Give a shortened derivation in λ2 to show that the following term is legal in the context Γ ≡ nat : ∗, bool : ∗ ;
   (λα, β : ∗. λf : α → α. λg : α → β. λx : α. g(f(fx))) nat bool.
 -/
 
 -- solution
 namespace ex_34
-def term_3_4 := (λ(α β : Type) => λ(f : α → α) => λ(g : α → β) => λ(x : α) => g (f (f x))) nat bool
+
+def term_3_4 {nat bool : Type} := (λ(α β : Type) => λ(f : α → α) => λ(g : α → β) => λ(x : α) => g (f (f x))) nat bool
 
 end ex_34
+
 
 /-
 Exercise 3.5
@@ -67,10 +74,13 @@ Let ⊥ ≡ Πα : ∗ . α and Γ ≡ β : ∗, x : ⊥.
 (c) Give three not β-convertible inhabitants of β → β in context Γ, each
 in β-normal form.
 (d) Prove that the following terms inhabit the same type in context Γ:
-λf : β → β → β . f (x β)(x β), x((β → β → β) → β)-/
+λf : β → β → β . f (x β)(x β), x((β → β → β) → β)
+-/
 
 namespace ex_35
-variable(x:Q) (β: Prop)
+section
+
+variable(x : Q) (β : Prop)
 
 -- solution for (a)
 def  Q :=  ∀ A : Prop, A
@@ -78,16 +88,64 @@ example : Prop := Q
 
 
 -- solution for (b)
-
 example : β := x β
 
 -- solution for (c)
-
 example : β → β := x (β → β)
 example : β → β := λ(y : β) => y
 example : β → β := λ(y : β) => x (β → β) y
 
+-- solution for (d)
 def term1 := λ(f : β → β → β) => f (x β) (x β)
 def term2 := x ((β → β → β) → β)
 
-example : (β → β → β) → β := term1
+example : (β → β → β) → β := term1 x β          -- Apply term1 to x and β "makes" the context Γ. It is necessary because def makes a "global term" with empty context
+example : (β → β → β) → β := term2 x β
+
+end
+end ex_35
+
+/-
+Exercise 3.6
+Find terms in ΛT2 that are inhabitants of the following λ2-types, each in
+the given context Γ:
+
+a) Π α,β : ∗ . (nat → α) → (α → nat → β) → nat → β, where Γ ≡ nat : ∗.
+b) Π δ : ∗ . ((α → γ) → δ) → (α → β) → (β → γ) → δ, where Γ ≡ α : ∗, β: ∗, γ: ∗.
+c) Π α,β,γ : ∗ . (α → (β → α) → γ) → α → γ, in the empty context.
+-/
+
+namespace ex_36
+
+-- solution for (a)
+def a {nat : Type} (α β : Type) (f : nat → α) (g : α → nat → β) (n : nat) :=  g (f n) n
+example : ∀ (α β : Type), (nat → α) → (α → nat → β) → nat → β := a
+
+-- solution for (b)
+def b {α β γ : Type} (δ : Type) (f : (α → γ) → δ) (g : α → β) (h : β → γ) := f (λ x : α => h (g x))
+example : ∀ (δ : Type), ((α → γ) → δ) → (α → β) → (β → γ) → δ := b
+
+-- solution for (c)
+def c (α β γ : Type) (f : α → (β → α) → γ) (x : α) := (f x) (λ _ : β => x)
+example : ∀ (α β γ : Type), (α → (β → α) → γ) → α → γ := c
+
+end ex_36
+
+/-
+Exercise 3.7
+Take ⊥ as in Exercise 3.5.
+Let context Γ be α : ∗, β : ∗, x : α → ⊥, f : (α → α) → α.
+Give a derivation to successively calculate an inhabitant of α and an
+inhabitant of β, both in context Γ.
+-/
+namespace ex_37
+
+
+variable (α β : Prop) (x : α → ex_35.Q) (f : (α → α) → α)
+
+-- inhabitant of α
+example : α := f (λ y : α => y)
+
+-- inhabitant of β
+def a := f (λ y : α => y)
+example : β := x (a α f) β

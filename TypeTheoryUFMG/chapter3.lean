b8a12f94e2a015740f43688c355af7d0eb1e61bf
TSF-Guilherme <ghds@aluno.ifnmg.edu.br>
2025-09-10 22:16:29 -0300

    Added exercises up to 3.13

--- TypeTheoryUFMG/chapter3.lean
+++ TypeTheoryUFMG/chapter3.lean
@@ -28,9 +28,9 @@ end ex_32
 -/
 
 namespace ex_33
-section
+section -- Section is used to define variables locally
 
-variable (nat bool : Type) (suc : nat → nat) (even : nat → bool)
+variable (nat bool : Type) (suc : nat → nat) (even : nat → bool) -- this variables cannot be used outside this section
 
 def M (α β γ : Type) (f : α → β) (g : β → γ) (x : α) := g (f x)
 
@@ -139,7 +139,7 @@ Give a derivation to successively calculate an inhabitant of α and an
 inhabitant of β, both in context Γ.
 -/
 namespace ex_37
-
+section
 
 variable (α β : Prop) (x : α → ex_35.Q) (f : (α → α) → α)
 
@@ -147,5 +147,156 @@ variable (α β : Prop) (x : α → ex_35.Q) (f : (α → α) → α)
 example : α := f (λ y : α => y)
 
 -- inhabitant of β
-def a := f (λ y : α => y)
+def a := f (λy : α => y)
 example : β := x (a α f) β
+
+end
+end ex_37
+
+/-
+Exercise 3.8
+Recall that K ≡ λ xy. x in untyped lambda calculus.
+Consider the following types:
+T1 ≡ Π α, β : ∗ . α → β → α and T2 ≡ Π α : ∗ . α → (Π β : ∗. β → α).
+Find inhabitants t1 and t2 of T1 and T2, which may be considered as
+different closed λ2-versions of K.
+-/
+
+-- solution
+namespace ex_38
+
+def T1 := ∀(α β : Type), α → β → α
+def T2 := ∀(α : Type), α → (∀ (β : Type), β → α)
+
+def M1 (α β : Type) (x : α) (_ : β) := x
+def M2 (α : Type) (x : α) (β : Type) (_ : β) := x
+
+#check (M1 : T1)
+#check (M2 : T2)
+
+/- M1 ≡ (λα,β : ∗ . λx : α. λy : β . x) and M2 ≡ (λα : ∗ . (λx : α. (λβ : ∗ . (λy : β . x))))
+They are closed λ2-versions of k because in each of them we're adding types to the variables x and y of the untyped
+term k ≡ (λxy. x) in such a way that there are no free type variables on it. -/
+
+end ex_38
+
+
+/-
+Execise 3.9
+Find a closed λ2-version of S ≡ λxyz. xz(yz), and establish its type.
+-/
+
+-- solution
+namespace ex_39
+
+def S2 (α β γ : Type) (x : α → (β → γ)) (y : α → β) (z : α) := x z (y z)
+def T := ∀(α β γ : Type), (α → (β → γ)) →  (α → β) → (α) → γ
+
+#check (S2 : T)
+
+end ex_39
+
+/-
+Exercise 3.10
+Let M ≡ λx : (Πα : ∗ . α → α) . x(σ → σ)(xσ).
+  (a) Prove that M is legal in λ2.
+  (b) Find a term N such that MN is legal in λ2 and may be considered
+    to be a proper way of adding type information to (λx. xx)(λy. y).
+-/
+
+
+namespace ex_310
+section
+
+-- solution for (a)
+def M {σ : Type} := λx : (∀ α : Type, α → α) => x (σ → σ) (x σ)
+def T {σ : Type} := (∀α : Type, α → α) → σ → σ
+
+#check (M : T)
+
+/- Thus, in the context Γ ≡ σ : Type, M has type T. We have to add the context on definition of T because, in λ2, types must
+have all free type variables declared. -/
+
+-- solution for (b)
+def N (α : Type) (x : α) := x
+def T' {σ : Type} := σ → σ
+
+#check (M N : T')
+
+end
+end ex_310
+
+/-
+Exercise 3.11
+Take ⊥ as in Exercise 3.5. Prove that the following term is legal in the
+empty context:
+λx : ⊥ . x(⊥ → ⊥ → ⊥)(x(⊥ → ⊥)x)(x(⊥ → ⊥ → ⊥)xx).
+What is its type?
+-/
+
+-- solution
+namespace ex_311
+
+def Q := ex_35.Q
+def M (x : Q) := x (Q → Q → Q) (x (Q → Q) x) (x (Q → Q → Q) x x)
+
+
+#check (M : Q → Q)
+
+end ex_311
+
+/-
+Exercise 3.12
+As mentioned in Section 3.8, we have in λ2 the polymorphic Church
+numerals. They resemble the untyped Church numerals, as described in
+Exercises 1.10 and 1.13(b). For example:
+Nat ≡ Πα : ∗ . (α → α) → α → α,
+Zero ≡ λα : ∗ . λf : α → α. λx : α. x, having type Nat,
+One ≡ λα : ∗ . λf : α → α. λx : α. fx, with type Nat, as well,
+Two ≡ λα : ∗ . λf : α → α. λx : α. f(fx).
+We define Suc as follows as a λ2-term:
+Suc ≡ λn : Nat. λβ : ∗ . λf : β → β. λx : β. f(nβfx).
+Check that Suc acts as a successor function for the polymorphic Church
+numerals, by proving that Suc Zero =β One and Suc One =β Two.
+-/
+
+-- Solution
+namespace ex_312
+
+def Nat := ∀ (α : Type), (α → α) → α → α
+def Zero (α : Type) (_ : α → α) (x : α) := x
+def One (α : Type) (f : α → α) (x : α) := f x
+def Two (α : Type) (f : α → α) (x : α) := f (f x)
+def Suc (n : Nat) (β : Type) (f : β → β) (x : β) := f (n β f x)
+
+theorem T1 : Suc Zero = One := by
+  rfl
+
+theorem T2 : Suc One = Two := by
+  rfl
+
+end ex_312
+
+/-
+Exercise 3.13
+See the previous exercise.
+  (a) We define Add in λ2 as follows:
+    Add ≡ λm,n : Nat. λα : ∗ . λf : α → α. λx : Nat. mαf(nαfx).
+    Show that Add simulates addition, by evaluating Add One One
+  (b) Find a λ2-term Mult that simulates multiplication on Nat.
+-/
+
+namespace ex_313
+def Nat := ex_312.Nat
+def One := ex_312.One
+def Two := ex_312.Two
+
+-- solution for (a)
+def Add (m n : Nat) (α : Type) (f : α → α) (x : α) := m α f (n α f x)
+
+theorem T : Add One One = Two := by
+  rfl
+
+-- solution for (b)
+
+end ex_313
